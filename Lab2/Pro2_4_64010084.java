5f633a4d52282a235e8f3cd92ddffdc2cfc35a15
Nont <kunanont.nont@gmail.com>
2022-01-21 15:45:55 +0700

    Update

--- Lab2/Pro2_4_64010084.java
+++ Lab2/Pro2_4_64010084.java
@@ -1,33 +1,46 @@
 import java.util.Scanner;
 
 public class Pro2_4_64010084 {
-    
-    public static void main(String[] args) {
-        
-        Scanner sn = new Scanner(System.in);
-        String[] cityName = new String[3];
-        String[] question = {"first","second","third"};
-        for(int i = 0; i < 3; i++) {
 
-            System.out.printf("Enter the %s city : ",question[i]);
-            cityName[i] = sn.nextLine();
+    public static void main(String[] args) {
+
+        Scanner sn = new Scanner(System.in);
+        int num;
+        System.out.print("Enter the number of lines: ");
+        num = sn.nextInt();
+        while (!check(num)) {
+
+            System.out.println("Please Enter Only 1 - 15");
+            System.out.print("Enter the number of lines: ");
+            num = sn.nextInt();
         }
         sn.close();
-        cityName = sort(cityName);
-        System.out.printf("The three cities in alphabetical order are %s %s %s", cityName[0],cityName[1],cityName[2]);
+        for(int i = 0; i > -num; i--) {
+
+            for(int j = -num + 1; j <= num - 1; j++) {
+
+                if( i <= -Math.abs(j)) {
+
+                    System.out.printf("%d ",Math.abs(j)+1);
+                }
+                else System.out.print("  ");
+            }
+            System.out.print("\n");
+        }
     }
 
-    public static String[] sort(String[] text) {
-            
-        for(int i = 0; i < text.length; i++) {
-            for(int j = i + 1; j < text.length; j++) {
-                if(text[i].compareToIgnoreCase(text[j]) > 0) {
-                    String a = text[j];
-                    text[j] = text[i];
-                    text[j] = a;
-                }
+    public static boolean check(int n) {
+
+        try {
+
+            if(n < 1 || n > 15) {
+
+                throw new Exception();
             }
+        }catch(Exception e) {
+            
+            return false;
         }
-        return text;
+        return true;
     }
 }
