4d2d3c0b85910ea1c2d92f82b79810e4441372d0
Nont <kunanont.nont@gmail.com>
2022-02-01 09:45:53 +0700

    update

---
+++ Lab2/Pro2_3_64010084/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner sn = new Scanner(System.in);
+        String[] cityName = new String[3];
+        String[] question = {"first","second","third"};
+        for(int i = 0; i < 3; i++) {
+
+            System.out.printf("Enter the %s city : ",question[i]);
+            cityName[i] = sn.nextLine();
+        }
+        sn.close();
+        cityName = sort(cityName);
+        System.out.printf("The three cities in alphabetical order are %s %s %s", cityName[0],cityName[1],cityName[2]);
+    }
+
+    public static String[] sort(String[] text) {
+            
+        for(int i = 0; i < text.length; i++) {
+            for(int j = i + 1; j < text.length; j++) {
+                if(text[i].compareToIgnoreCase(text[j]) > 0) {
+                    String a = text[j];
+                    text[j] = text[i];
+                    text[i] = a;
+                }
+            }
+        }
+        return text;
+    }
+}
