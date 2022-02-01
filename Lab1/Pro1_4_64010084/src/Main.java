9f9980dfc0f7a1af9f6559bd40d2d90872aa3ebf
Nont <kunanont.nont@gmail.com>
2022-02-01 09:34:03 +0700

    update

---
+++ Lab1/Pro1_4_64010084/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+
+    final static double P_TO_KILO = 0.45359237;
+    final static double INCH_TO_METER =  0.0254;
+    public static void main (String args[]) {
+
+        Scanner sn = new Scanner(System.in);
+        double height,weight,bmi;
+        System.out.print("Enter weight in pounds:");
+        weight = sn.nextDouble();
+        System.out.print("Enter height in inches:");
+        height = sn.nextDouble();
+        sn.close();
+        height *= INCH_TO_METER;
+        weight *= P_TO_KILO;
+        bmi = weight / (height * height);
+        System.out.println("BMI is " + bmi);
+    }
+}
