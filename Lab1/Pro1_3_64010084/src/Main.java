2f511d17bae87cabcad42086cfc03be37a67ca07
Nont <kunanont.nont@gmail.com>
2022-02-01 09:30:37 +0700

    update

---
+++ Lab1/Pro1_3_64010084/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    
+    static int number,total,digit1,digit2,digit3;
+    public static void main (String args[]) {
+        
+        Scanner sn = new Scanner(System.in);
+        System.out.print("Enter a number between 0 and 1000: ");
+        number = sn.nextInt();
+        sn.close();
+        if (number < 0 || number > 1000) {
+
+            System.out.println("Out of Range");
+        }
+        else {
+
+           digit1 = number % 10;
+           number /= 10;
+           digit2 = number % 10;
+           number /= 10;
+           digit3 = number % 10;
+           total = digit1 + digit2 + digit3;
+           System.out.println("The sum of the digits is " + total);
+        }
+    }
+}
